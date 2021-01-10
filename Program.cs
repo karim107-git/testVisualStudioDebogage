3a2fa72a6f283f1322a7a070cfe6495c5bdadea2
karim107-git <karim107@yahoo.fr>
2021-01-10 14:21:22 -0500

    ajout des modification initialisation du projet

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace testDebogage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.Write("Introduire un nombre : ");
+            bool resultat=int.TryParse(Console.ReadLine(),out int myNumber);
+            
+            
+            
+            if (resultat)
+            {
+                maClasse classe = new maClasse("john", "nash", myNumber);
+                Console.WriteLine("Hello World!");
+                maClasse c = classe;
+                Console.WriteLine(c.ToString());
+                Console.WriteLine("tout est Ok");
+            }
+            else 
+            {
+                Console.WriteLine("Il faut entrer un entier pour te dire hello world");
+            }
+
+
+           
+        
+        }
+    }
+}
